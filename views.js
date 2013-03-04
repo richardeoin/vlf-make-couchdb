a06b88293b73c5ceea6f7b1b883c4469b0ca1a79
Richard Eoin Meadows <richardeoin@gmail.com>
2013-03-04 19:49:23 +0000

    Added Base 64 decode for testing, and another view (bintest) to test this.

--- views.js
+++ views.js
@@ -5,13 +5,26 @@
 exports = module.exports = function() {
 	/* ======== Decode Function ======== */
 
+	/**
+	 * Returns a function that decodes the 'bin' field in a document into a human readable document.
+	 */
 	function get_decode_function() {
-	
+		/* Return a stringified function */
+		return (function (doc) {
+			return decode_base64(doc.bin);
+		}).toString().replace(/decode_base64/g, require('./base64').toString());
 	}
 
 	/* ======== Main ======== */
 
 	return {
+		bintest: {
+			map: (function (doc) {
+				if (doc.bin) {
+					emit(doc._id, decode(doc));
+				}
+			}).toString().replace(/decode/g, get_decode_function())
+		},
 		left: {
 			map: (function (doc) {
 				if (doc.bin) { /* If the document is still in binary format */
@@ -33,13 +46,16 @@ exports = module.exports = function() {
 			}).toString().replace(/decode/g, get_decode_function())
 		},
 		battery: {
-			map: function (doc) {
+			map: (function (doc) {
+				if (doc.bin) { /* If the document is still in binary format */
+					doc = decode(doc); /* Decode it */
+				}
 				if (doc.battery && doc.battery.value) {
 					var adc_value = doc.battery.value/10;
 					var pin_voltage = (adc_value*1.8)/1024;
 					emit(doc.time, Math.round(pin_voltage*11*1000)/1000);
 				}
-			}
+			}).toString().replace(/decode/g, get_decode_function())
 		}
 	};
 }();
