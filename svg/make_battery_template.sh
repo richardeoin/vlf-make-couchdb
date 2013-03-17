c367fd3fd2a7e7b854ce4db4362a3b120b11108a
Richard Eoin Meadows <richardeoin@gmail.com>
2013-03-17 10:18:06 +0000

    Bash scripts for SVG generation now always output SVGs in the correct directory

--- svg/make_battery_template.sh
+++ svg/make_battery_template.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-svg_plot_gen --x-text "Time" --y-text "Battery Voltage (V)" -tc > plot_battery_template.svg
+svg_plot_gen --x-text "Time" --y-text "Battery Voltage (V)" -tc > "`dirnanme $0`/plot_battery_template.svg"
